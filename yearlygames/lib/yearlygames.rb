f2a0a490be9d85646996eda2121f9fc1dd6a3c32
Robert Salaiz <work.salaizrm@gmail.com>
2020-09-10 00:57:22 +0000

    Automatically backed up by Learn

--- yearlygames/lib/yearlygames.rb
+++
@@ -1,6 +0,0 @@
-require "yearlygames/version"
-
-module Yearlygames
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- yearlygames/lib/yearlygames/version.rb
+++
@@ -1,3 +0,0 @@
-module Yearlygames
-  VERSION = "0.1.0"
-end
